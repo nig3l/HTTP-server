98462964d1445620be5405a7d7fd1bc36eec4718
nigel <nigelchimwene@gmail.com>
2025-03-23 15:13:53 +0300

    fixed routing error

--- server.go
+++ server.go
@@ -29,16 +29,13 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-    http.HandleFunc("/", pathHandler)
-    http.HandleFunc("/home", homeHandler)
-
-    http.HandleFunc("/", logRequest(func(w http.ResponseWriter, r *http.Request) {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-    }))
+    })
 
-    log.Println("Starting server...")
-    err := http.ListenAndServe("8080", nil)
+    log.Println("Starting server on :8080")
+    err := http.ListenAndServe(":8080", nil)
     if err != nil {
-        log.Fatalf("Could not start server: %v", err)
+        log.Fatalf("Failed to start server: %v", err)
     }
 }
