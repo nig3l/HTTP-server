a5793b430918dd10d855ec1fa77b25561671bac5
nigel <nigelchimwene@gmail.com>
2025-03-23 15:06:40 +0300

    initial commit

---
+++ server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+    "time"
+    
+)
+
+func logRequest(next http.HandlerFunc) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        startTime := time.Now()
+        next(w, r)
+        log.Printf("%s %s %s", r.Method, r.URL.Path, time.Since(startTime))
+    }
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+        http.NotFound(w, r)
+        return
+    }
+    fmt.Fprintf(w, "Welcome to the home page!")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "You've requested: %s\n", r.URL.Path)
+}
+
+func main() {
+    http.HandleFunc("/", pathHandler)
+    http.HandleFunc("/home", homeHandler)
+
+    http.HandleFunc("/", logRequest(func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+    }))
+
+    log.Println("Starting server...")
+    err := http.ListenAndServe("8080", nil)
+    if err != nil {
+        log.Fatalf("Could not start server: %v", err)
+    }
+}
